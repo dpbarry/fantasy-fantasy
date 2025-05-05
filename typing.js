1314bf4c5c7f2c27bb56172fa93ee0c3c4d64c8a
dpbarry <deanbarry100@gmail.com>
2025-05-05 14:36:41 -0400

    Support input shadowtyping

--- typing.js
+++ typing.js
@@ -33,12 +33,73 @@ export function typeP(text, body, callback = (p) => {}) {
     typeOn(text, p, () => callback(p));
 }
 
+
+// expects a string with @ representing inputs
+export async function typePWithInputs(body, string, ids, cb, externalCB) {
+    let p = document.createElement("p");
+    body.appendChild(p);
+    [...string].forEach(c => {
+        let span = document.createElement("span");
+        if (c === "@") {
+            span.className = "inputwrap";
+        } else {
+            span.style.color = "transparent";
+            span.style.pointerEvents = "none";
+            span.style.userSelect = "none";
+            span.innerText = c;
+        }
+        p.appendChild(span);
+    });
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const waitForEvent = (el, ev) => new Promise(res =>
+        el.addEventListener(ev, function h(e) {
+            el.removeEventListener(ev, h);
+            res(e);
+        })
+    );
+
+    let inputIndex = 0;
+    let inputs = [];
+
+    for (const [idx, span] of [...p.children].entries()) {
+        await delay(15);
+
+        if (span.className === "inputwrap") {
+            const input = document.createElement("input");
+            input.id           = ids[inputIndex++];
+            input.autocomplete = "off";
+            input.spellcheck   = false;
+            input.oninput      = cb;
+            input.onanimationend = () => input.classList.remove("invalid");
+
+            inputs.push(input);
+            span.appendChild(input);
+
+            await waitForEvent(input, "transitionend");
+        }
+        else {
+            Object.assign(span.style, {
+                color:         "",
+                pointerEvents: "",
+                userSelect:    ""
+            });
+        }
+    }
+
+    externalCB(p, inputs);
+}
+
+
+
+
+
 export function collapseP(p, opt = () => {}) {
     let text = [...p.children].reduce( (acc, x) => {
-        if (x.nodeName === "SPAN")
+        if (!x.className.trim())
             return acc + x.innerHTML;
         else
-            return acc + opt(x.value);
+            return acc + opt(x.firstChild.value);
     }, "");
     p.innerHTML = text;
 }
