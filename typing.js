75199abac9b20c8f11ab5e558896d95eb7933684
dpbarry <deanbarry100@gmail.com>
2025-05-06 18:03:29 -0400

    Add next form

--- typing.js
+++ typing.js
@@ -1,6 +1,38 @@
-
-export function typeOn(text, body, callback) {
+const TYPE_DELAY = 10;
+export async function typeOn(text, body, callback = () => {}) {
     // first add all the characters invisibly to establish justified alignment
+    await hideText(text, body);
+
+    // now gradually "type" each character
+    await renderText(body);
+
+    // run callback, if any, after last character has been typed
+    callback();
+}
+
+// convenience function to abstract the <p> created in a message
+export async function typeP(text, body, callback = () => {}) {
+    let p = document.createElement("p");
+    body.appendChild(p);
+    await typeOn(text, p, () => callback(p));
+}
+
+async function renderText(p) {
+    for (const span of [...p.children]) {
+        if (span.classList.contains("deep")) {
+            await renderText(span);
+            span.classList.add("visible");
+        } else {
+            await delay(TYPE_DELAY);
+            Object.assign(span.style, {
+                color:         "",
+                pointerEvents: "",
+                userSelect:    ""
+            });
+        }
+    }
+}
+function hideText(text, body) {
     [...text].forEach(c => {
         let span = document.createElement("span");
         span.innerText = c;
@@ -9,36 +41,44 @@ export function typeOn(text, body, callback) {
         span.style.userSelect = "none";
         body.appendChild(span);
     });
-
-    // now gradually "type" each character
-    let count = 0;
-    while (count < text.length) {
-        let span = body.children[count++];
-        setTimeout ( () => {
-            span.style.color = "";
-            span.style.pointerEvents = "";
-            span.style.userSelect = "";
-
-        }, count * 15);
-    }
-
-    // run callback, if any, after last character has been typed
-    setTimeout(callback, 15 * text.length);
 }
 
-// convenience function to abstract the <p> created in a message
-export function typeP(text, body, callback = (p) => {}) {
+export async function typePWithSpans(text, body, spanIDs, spanTexts, callback = () => {}) {
     let p = document.createElement("p");
     body.appendChild(p);
-    typeOn(text, p, () => callback(p));
+    let spans = [];
+    
+    let count = 0;
+    [...text].forEach(c => {
+        let span = document.createElement("span");
+        if (c === "@") {
+            span.classList.add("deep");
+            span.id = spanIDs[count];
+            hideText(spanTexts[count++], span);
+            spans.push(span);
+        } else {
+            span.style.color = "transparent";
+            span.style.pointerEvents = "none";
+            span.style.userSelect = "none";
+            span.innerText = c;
+        }
+        p.appendChild(span);
+
+    });
+
+    await renderText(p);
+
+    
+    p.querySelectorAll(".deep").forEach(collapseP);
+    return spans;
 }
 
 
 // expects a string with @ representing inputs
-export async function typePWithInputs(body, string, width, ids, cb, externalCB) {
+export async function typePWithInputs(body, text, width, ids, cb, externalCB) {
     let p = document.createElement("p");
     body.appendChild(p);
-    [...string].forEach(c => {
+    [...text].forEach(c => {
         let span = document.createElement("span");
         if (c === "@") {
             span.className = "inputwrap";
@@ -52,19 +92,12 @@ export async function typePWithInputs(body, string, width, ids, cb, externalCB) 
         p.appendChild(span);
     });
 
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-    const waitForEvent = (el, ev) => new Promise(res =>
-        el.addEventListener(ev, function h(e) {
-            el.removeEventListener(ev, h);
-            res(e);
-        })
-    );
 
     let inputIndex = 0;
     let inputs = [];
 
-    for (const [idx, span] of [...p.children].entries()) {
-        await delay(15);
+    for (const span of [...p.children]) {
+        await delay(TYPE_DELAY);
 
         if (span.className === "inputwrap") {
             const input = document.createElement("input");
@@ -103,12 +136,21 @@ export async function typePWithInputs(body, string, width, ids, cb, externalCB) 
 
 
 
-export function collapseP(p, opt = () => {}) {
+export function collapseP(p, opt = (x) => {x.firstChild.value}) {
     let text = [...p.children].reduce( (acc, x) => {
         if (!x.className.trim())
             return acc + x.innerHTML;
         else
-            return acc + opt(x.firstChild.value);
+            return acc + opt(x);
     }, "");
     p.innerHTML = text;
+    return p;
 }
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const waitForEvent = (el, ev) => new Promise(res =>
+    el.addEventListener(ev, function h(e) {
+        el.removeEventListener(ev, h);
+        res(e);
+    })
+);
